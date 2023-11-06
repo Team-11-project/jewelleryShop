853c22eb6bb3e30155d1f9dd6bc84ee905f524b7
Rukayat-T <tewogbaderukayat@gmail.com>
2023-11-06 15:23:18 +0000

    insatlled dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,7 +13,6 @@ import { type } from 'os';
       host: process.env.POSTGRES_HOST,
       port: parseInt(<string>process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       autoLoadEntities: true,
       synchronize: true,
