12ad4a2a05b72b63038fa1b9b80bf5006af97d28
Rojdeep Singh <210137910@aston.ac.uk>
2023-11-14 12:04:33 +0000

    home

---
+++ Frontend/src/homePagr/home.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function Home() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1>hello world from home</h1>
+    </>
+  )
+}
+
+export default Home
