1dc0c0dc3805aba5c84710da599e0bdcb590c350
Rukayat-T <tewogbaderukayat@gmail.com>
2023-11-20 17:43:24 +0000

    login frontend ui

---
+++ Frontend/src/Pages/Login/Login.jsx
@@ -0,0 +1,74 @@
+import React, { useContext } from 'react'
+import AuthContext from '../../Context/AuthContext';
+import './loginStyles.css'
+
+function Login() {
+    let { loginUser } = useContext(AuthContext);
+
+    return (
+        <>
+            <div className="outlet">
+                <div className="leftSide">
+                    <div className="logo"> Regalia </div>
+                    <div className="fr">
+                        <div className="text">
+                            <div className="text-head">Responsibly crafted jewellery</div>
+                            <div className="text-small">True Luxury in jewellery</div>
+                        </div>
+                    </div>
+                </div>
+                <div className="rightSide">
+                    <div className="log-head">
+                        <div className="log">Log into your account</div>
+                        <div className="acc">Don't have an account? <a href="#">Sign up</a></div>
+                    </div>
+
+                    <div className="mid">
+                        <div className="inWith">Log in with</div>
+                        <div className="social-login">
+                            <div className="box1"></div>
+                            <div className="box1"></div>
+                            <div className="box1"></div>
+                        </div>
+                        <div className="divider">
+                            <div className="line"></div>
+                            <div className="or">OR</div>
+                            <div className="line"></div>
+                        </div>
+                    </div>
+
+
+                    <div className="form">
+                        <div className="entry">
+                            <label>Email:</label>
+                            <input
+                                className="border-2 border-black rounded-md"
+                                type="text"
+                                name="email"
+                            />
+                        </div>
+                        <div className="entry">
+                            <label>Password:</label>
+                            <input
+                                className="border-2 border-black rounded-md"
+                                type="password"
+                                name="password"
+                            />
+                        </div>
+                        <div className="rem">
+                            <input type="checkbox" className='cb' /> Remember me
+                        </div>
+                        <div className="FPass">Forgot Password?</div>
+                        <div className="btn">
+                            <button>Log In</button>
+                        </div>
+
+
+                    </div>
+                </div>
+            </div >
+        </>
+    )
+}
+
+export default Login
