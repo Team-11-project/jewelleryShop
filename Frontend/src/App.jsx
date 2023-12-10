d76159c813c73b3df11aa0d3b2df176259eeaf74
GurnavrajSingh <gnavjsingh@gmail.com>
2023-12-10 22:24:28 +0000

    fixed App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Outlet, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './homePage/home';
@@ -11,43 +11,56 @@ import Footer from './assets/footer';
 import Products from './productsPage/products';
 import AddCart from './addCart/addCart';
 import { AuthProvider } from './Context/AuthContext';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ForgotPassword from './forgotPassword/forgotPassword';
 import Dashboard from './AdminSide/Pages/Dashboard/Dashboard';
 import Overview from './AdminSide/Pages/Dashboard/Overview/Overview';
+import IndividualProduct from './productsPage/individualProducts';
+import AddCartPage from './addCart/addCartPage';
+import CheckoutPage from './Checkout/checkout';
+import ProductsAdmin from './AdminSide/Pages/Dashboard/Products/Products';
+import Signup from './Signup/Signup'
+import AdminSignUp from './Signup/AdminSignUp'
 
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faHome, faTh, faInfo, faStar } from '@fortawesome/free-solid-svg-icons';
 
 library.add(faHome, faTh, faInfo, faStar);
 
-const MainLayout = ({ children }) => (
-  <>
-    {children}
-    <Footer />
-  </>
-);
-
 function App() {
+
+  const currentURL = window.location.pathname
+  console.log(currentURL)
+
   return (
     <Router>
       <AuthProvider>
+        {/* {
+          currentURL === "/dashboard" || "/login" || "/forgotPassword" ? <></> : <AppNavbar />
+        } */}
+        {/* {
+          currentURL === "/" ? <AppNavbar /> : <></>
+        } */}
+
         <Routes>
-          <Route
-            path="/"
-            element={<MainLayout><Home /></MainLayout>}
-          />
-          <Route path='/login' element={<MainLayout><Login /></MainLayout>} />
-          <Route path='/forgotPassword' exact element={<MainLayout><ForgotPassword /></MainLayout>} />
-          <Route path='/dashboard' exact element={<MainLayout><Dashboard /></MainLayout>} />
-          <Route path='/products' element={<MainLayout><Products /></MainLayout>} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/forgotPassword' exact element={<ForgotPassword />} />
+          <Route path='/dashboard' exact element={<Dashboard />} />
+          <Route path='/checkout' exact element={<CheckoutPage />} />
+          <Route path='/products' element={<Products />} />
           <Route path='/contact' element={<Contact />} />
-          <Route path='/about' element={<MainLayout><About /></MainLayout>} />
-          <Route path="/addCart" element={<MainLayout><AddCart /></MainLayout>} />
+          <Route path='/about' element={<About />} />
+          <Route path="/addCart" element={<AddCart />} />
+          <Route path='/product/:productId' element={<IndividualProduct />} />
+          <Route path="/cart" element={<AddCartPage />} />
+          <Route path='/Signup' exact element={<Signup />} />
+          <Route path='/AdminSignUp' exact element={<AdminSignUp />} />
         </Routes>
+        {
+          currentURL === "/dashboard" || "/login" || "/forgotPassword" ? <></> : <Footer />
+        }
       </AuthProvider>
     </Router>
-  );
+  )
 }
-
 export default App;
