0daa493449b9c226b427f54bae1572e6e8bcc993
asingh2004 <220042769@aston.ac.uk>
2024-03-14 11:49:38 +0000

    Added filter button for products

--- Frontend/src/productsPage/products.jsx
+++ Frontend/src/productsPage/products.jsx
@@ -3,113 +3,83 @@ import { Container, Row, Col, Dropdown, Form, Card, Button } from 'react-bootstr
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faShoppingBag, faFilter, faSort } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
-import img1 from './img1.jpg';
-import rolexOyster from './rolexOyster.jpg'
 import './products.css';
-import AppNavbar from '../assets/navbar';
-import AuthContext from '../Context/AuthContext';
+import AppNavbar from '../assets/navbar'; // Adjust the path as necessary
+import AuthContext from '../Context/AuthContext'; // Adjust the path as necessary
 
 function Products() {
-  let { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   const [selectedPrice, setSelectedPrice] = useState(null);
   const [selectedMaterial, setSelectedMaterial] = useState(null);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedSort, setSelectedSort] = useState(null);
-  const [cart, setCart] = useState([]);
   const [showFilters, setShowFilters] = useState(false);
-  const [AllProducts, setAllProducts] = useState([])
+  const [AllProducts, setAllProducts] = useState([]);
 
-
-  const getProducts = async () => {
-    try {
-      // setIsLoading(true)
-      let response = await fetch(`http://localhost:3001/products/get-all-products`,
-        {
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        let response = await fetch(`http://localhost:3001/products/get-all-products`, {
           method: "GET",
           headers: {
             'Content-Type': 'application/json',
-          }
-        })
-      const resJson = await response.json();
-      if (response.status === 200) {
-        console.log(resJson, "response")
-        setAllProducts(resJson.response);
-      } else {
-        console.log(resJson);
-        alert("error: " + resJson.message)
+          },
+        });
+        const resJson = await response.json();
+        if (response.status === 200) {
+          setAllProducts(resJson.response);
+        } else {
+          alert("error: " + resJson.message);
+        }
+      } catch (error) {
+        console.log(error);
       }
-    }
-    catch (error) {
-      // setIsLoading(true)
-      console.log(error)
-    }
-  }
+    };
 
-  const addToCart = async (productId) => {
-   
-    try {
-      // setIsLoading(true)
-      const userId = user.user.id
-      let response = await fetch(`http://localhost:3001/cart/add/${userId}/${productId}`,
-        {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json',
-          }
-        })
-      // const resJson = await response.json();
-    }
-    catch (error) {
-      // setIsLoading(true)
-      console.log(error)
-    }
-  }
-
-  useEffect(() => {
-    getProducts()
-  }, [])
-
-  // const products = [
-  //   { id: 1, name: 'Rolex Oyster Perpetual', price: 8000, image: rolexOyster },
-  //   { id: 2, name: 'Product 2', price: 30, image: img1 },
-  //   { id: 3, name: 'Product 2', price: 30, image: img1 },
-  //   { id: 4, name: 'Product 2', price: 30, image: img1 },
-  //   { id: 5, name: 'Product 2', price: 30, image: img1 },
-  //   { id: 6, name: 'Product 2', price: 30, image: img1 },
-  //   { id: 7, name: 'Product 2', price: 30, image: img1 },
-  //   { id: 8, name: 'Product 2', price: 30, image: img1 },
-  //   { id: 9, name: 'Product 2', price: 30, image: img1 },
-  // ];
+    getProducts();
+  }, []);
 
   const priceOptions = ['100-500', '500-1000', '1000-5000', '5000+'];
   const materialOptions = ['Gold', 'Silver', 'Diamond', 'Gemstone'];
   const categoryOptions = ['Earrings', 'Watches', 'Necklaces', 'Bracelets'];
   const sortOptions = ['Recommend', 'New Arrivals', 'Price Low to High', 'Price High to Low'];
 
-  // const addToCart = (product) => {
-  //   setCart([...cart, product]);
-  // };
+  const handleToggleFilters = () => setShowFilters(!showFilters);
 
-  const handleToggleFilters = () => {
-    setShowFilters(!showFilters);
+  const inPriceRange = (product) => {
+    if (!selectedPrice) return true;
+    const price = parseInt(product.price, 10);
+    const [min, max] = selectedPrice.split('-').map(Number);
+    if (max) {
+      return price >= min && price <= max;
+    }
+    return price >= min;
   };
 
-  const handleHeartClick = (product) => {
-    // Handle heart icon click, e.g., add to favorites/liked
-    console.log(`Added ${product.name} to favorites/liked`);
-  };
+  const filteredProducts = AllProducts.filter(product =>
+    inPriceRange(product) &&
+    (!selectedMaterial || product.material === selectedMaterial) &&
+    (!selectedCategory || product.category === selectedCategory)
+  );
 
-  const handleShoppingBagClick = (product) => {
-    // Handle shopping bag icon click, e.g., add to cart
-    addToCart(product);
-    console.log(`Added ${product.name} to the shopping bag`);
-  };
+  const sortedProducts = filteredProducts.sort((a, b) => {
+    switch (selectedSort) {
+      case 'Price Low to High':
+        return a.price - b.price;
+      case 'Price High to Low':
+        return b.price - a.price;
+      case 'New Arrivals':
+        // Implement logic for sorting by new arrivals if available data supports it
+        return 0; // Placeholder
+      default:
+        return 0; // No sorting or default sorting logic here
+    }
+  });
 
   return (
     <>
       <AppNavbar />
       <Container className="products-container">
-
         <Row>
           <Col>
             <div className="products-intro">
@@ -127,6 +97,7 @@ function Products() {
 
           {showFilters && (
             <div className="filter-options">
+              {/* Price Filter */}
               <Form.Group className="filter-group">
                 <Form.Label>Price:</Form.Label>
                 <Dropdown onSelect={(eventKey) => setSelectedPrice(eventKey)}>
@@ -143,6 +114,7 @@ function Products() {
                 </Dropdown>
               </Form.Group>
 
+              {/* Material Filter */}
               <Form.Group className="filter-group">
                 <Form.Label>Material:</Form.Label>
                 <Dropdown onSelect={(eventKey) => setSelectedMaterial(eventKey)}>
@@ -159,6 +131,7 @@ function Products() {
                 </Dropdown>
               </Form.Group>
 
+              {/* Category Filter */}
               <Form.Group className="filter-group">
                 <Form.Label>Category:</Form.Label>
                 <Dropdown onSelect={(eventKey) => setSelectedCategory(eventKey)}>
@@ -175,6 +148,7 @@ function Products() {
                 </Dropdown>
               </Form.Group>
 
+              {/* Sort Filter */}
               <Form.Group className="filter-group">
                 <Form.Label>Sort By:</Form.Label>
                 <Dropdown onSelect={(eventKey) => setSelectedSort(eventKey)}>
@@ -196,7 +170,7 @@ function Products() {
 
         <div className="product-display">
           <div className="card-container">
-            {AllProducts.map((product) => (
+            {sortedProducts.map((product) => (
               <Card key={product.productId}>
                 <Link to={`/product/${product.productId}`} state={product}>
                   <Card.Img variant="top" src={product.image} />
@@ -205,10 +179,10 @@ function Products() {
                   <Card.Title>{product.name}</Card.Title>
                   <Card.Text>£{product.price}</Card.Text>
                   <div className="card-icons">
-                    <a href="#" onClick={() => handleHeartClick(product)}>
+                    <a href="#!" onClick={() => { /* Add to Favorites logic */ }}>
                       <FontAwesomeIcon icon={faHeart} className="icon" style={{ color: 'rgb(0, 1, 59)' }} />
                     </a>
-                    <Link to="/addCart" onClick={() => addToCart(product?.productId)}>
+                    <Link to="/cart" onClick={() => { /* Add to Cart logic */ }}>
                       <FontAwesomeIcon icon={faShoppingBag} className="icon" style={{ color: 'rgb(0, 1, 59)' }} />
                     </Link>
                   </div>
@@ -219,6 +193,6 @@ function Products() {
         </div>
       </Container>
     </>
-  )
+  );
 }
 export default Products;
